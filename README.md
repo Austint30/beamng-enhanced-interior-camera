3d77ed499e1d4790b2df9dafc042c770b7f3fd42
Austin Thibodeaux <austinthibodeaux@gmail.com>
2024-12-15 01:41:35 -0600

    Added -Install parameter to package script. Updated README.

--- README.md
+++ README.md
@@ -2,7 +2,32 @@
 
 Mod link: https://www.beamng.com/resources/enhanced-interior-camera.24952
 
-# Packaging for BeamNG
+## Project Setup
 
-In order for this mod to be usable for BeamNG, you will need to create a zip file that contains the src files.
+### Requirements
+
+- Powershell 7 and greater
+- Git
+
+To set-up this project in BeamNG, navigate to your BeamNG mods folder and open a terminal inside of the `unpacked` folder.
+
+Run `git clone https://github.com/Austint30/beamng-enhanced-interior-camera.git` to download the code from GitHub.
+
+Now you should be able access the mod in BeamNG. If you make any code changes, make sure to press `Ctrl + L` in-game to reload the game engine LUA.
+
+## Packaging for BeamNG
+
+To package this mod for upload in the repository, you will need to create a zip file that contains the source files.
 To simplify this process you can run `./package.ps1` with Powershell and a zip file will be generated for you in the `dist` folder.
+
+For your convenience, you can optionally provide an `-Install` parameter so that the zip file will be automatically moved to your BeamNG mods folder.
+
+### Example:
+
+```
+./package.ps1 -Install
+```
+
+## Notes
+
+- Using an older version of powershell will create zip files that BeamNG cannot read for whatever reason. Use powershell 7 and above to be safe.
--- package.ps1
+++ package.ps1
@@ -1,38 +1,81 @@
-# Define the paths
-$scriptPath = $PSScriptRoot
-$distPath = Join-Path -Path $scriptPath -ChildPath "dist"
-$zipPath = Join-Path -Path $distPath -ChildPath "enhanceddriver.zip"
+<#
+.SYNOPSIS
+    Packages the Enhanced Interior Camera mod into a zip file.
 
-# Source files to be zipped
-$srcFiles = @(
+.PARAMETER Install
+    Optional parameter. If specified, the script will place the zip file in the mods folder
+    in your game's user folder.
+
+.EXAMPLE
+    .\package.ps1
+    Creates the zip file in the 'dist' directory.
+
+.LINK
+    https://github.com/Austint30/beamng-enhanced-interior-camera
+
+.EXAMPLE
+    .\package.ps1 -Install
+    Creates the zip file in the mods folder.
+#>
+
+param (
+    [switch]$Install
+)
+
+$modBaseName = "enhanceddriver"
+
+$scriptPath = $PSScriptRoot
+$scriptFolder = Get-Item -Path $scriptPath
+$projectName = $scriptFolder.Name
+
+$distPath = Join-Path -Path $scriptPath -ChildPath "dist"
+$zipPath = Join-Path -Path $distPath -ChildPath "$modBaseName.zip"
+
+# Source folders to be zipped
+$srcFolders = @(
     "lua",
     "settings",
     "ui"
 )
 
-# Clear the dist folder if it exists
-if (Test-Path -Path $distPath) {
-    Remove-Item -Path $distPath -Recurse -Force
+# Function to create zip file
+function Create-Zip($destinationPath) {
+    try {
+        $files = $srcFolders | ForEach-Object { Join-Path -Path $scriptPath -ChildPath $_ }
+        Compress-Archive -Path $files -DestinationPath $destinationPath -CompressionLevel Optimal -Force
+    } catch {
+        Write-Host "An error occurred while creating the zip file: $_"
+        exit 1
+    }
 }
 
-# Create the dist folder
-New-Item -Path $distPath -ItemType Directory
+if ($Install) {
+    # Get parent directories
+    $unpackedFolder = $scriptFolder.Parent
 
-# Create a temporary folder for zipping
-Add-Type -AssemblyName 'System.IO.Compression.FileSystem'
-$zipTempFolder = Join-Path -Path $scriptPath -ChildPath "zip_temp"
-New-Item -Path $zipTempFolder -ItemType Directory
+    # Check if the parent is "unpacked"
+    if ($unpackedFolder.Name -eq "unpacked") {
+        $modsFolder = $unpackedFolder.Parent
+        $modsPath = $modsFolder.FullName
+        # Define the installation path
+        $installPath = Join-Path -Path $modsPath -ChildPath "$modBaseName.zip"
+        # Create the zip file at the installation path
+        Create-Zip -destinationPath $installPath
+        Write-Host "Mod packaged successfully and installed into mods folder."
+    } else {
+        Write-Host "Project must be placed in the 'unpacked' directory of your BeamNG mods folder to install."
+    }
+} else {
+    # Clear the dist folder if it exists
+    if (Test-Path -Path $distPath) {
+        Remove-Item -Path $distPath -Recurse -Force
+    }
 
-# Copy folders to a temporary location for zipping
-foreach ($item in $srcFiles) {
-    $folderPath = Join-Path -Path $scriptPath -ChildPath $item
-    Copy-Item -Path $folderPath -Destination $zipTempFolder -Recurse
+    # Create the dist folder
+    New-Item -Path $distPath -ItemType Directory
+
+    # Create the zip file in the dist folder
+    Create-Zip -destinationPath $zipPath
+
+    Write-Host "Mod packaged successfully"
 }
-
-# Create the zip file from the temporary location
-[System.IO.Compression.ZipFile]::CreateFromDirectory($zipTempFolder, $zipPath)
-
-# Clean up the temporary location
-Remove-Item -Path $zipTempFolder -Recurse -Force
-
-Write-Host "Mod packaged successfully"
